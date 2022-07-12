93154819a6f32bd5e27f11d64dc4a11bc4e3e282
Ichinose Shogo <shogo82148@gmail.com>
2022-07-12 17:41:00 +0900

    improve tests of Maker

--- maker_test.go
+++ maker_test.go
@@ -3,6 +3,8 @@ package myddlmaker
 import (
 	"bytes"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 type Foo1 struct {
@@ -28,13 +30,13 @@ func (*Foo2) Indexes() []Index {
 	}
 }
 
-func TestMaker(t *testing.T) {
+func testMaker(t *testing.T, structs []any, ddl string) {
 	m, err := New(&Config{})
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	m.AddStructs(&Foo1{})
+	m.AddStructs(structs...)
 
 	var buf bytes.Buffer
 	if err := m.Generate(&buf); err != nil {
@@ -42,42 +44,27 @@ func TestMaker(t *testing.T) {
 	}
 
 	got := buf.String()
-	want := "SET foreign_key_checks=0;\n" +
-		"DROP TABLE IF EXISTS `foo1`;\n\n" +
-		"CREATE TABLE `foo1` (\n" +
-		"    `id` INTEGER NOT NULL,\n" +
-		"    PRIMARY KEY (`id`)\n" +
-		") ENGINE=InnoDB DEFAULT CHARACTER SET = 'utf8mb4';\n\n" +
-		"SET foreign_key_checks=1;\n"
-	if got != want {
-		t.Errorf("want %q, got %q", want, got)
+	if diff := cmp.Diff(ddl, got); diff != "" {
+		t.Errorf("ddl is not match: (-want/+got)\n%s", diff)
 	}
 }
 
-func TestMaker2(t *testing.T) {
-	m, err := New(&Config{})
-	if err != nil {
-		t.Fatal(err)
-	}
+func TestMaker(t *testing.T) {
+	testMaker(t, []any{&Foo1{}}, "SET foreign_key_checks=0;\n"+
+		"DROP TABLE IF EXISTS `foo1`;\n\n"+
+		"CREATE TABLE `foo1` (\n"+
+		"    `id` INTEGER NOT NULL,\n"+
+		"    PRIMARY KEY (`id`)\n"+
+		") ENGINE=InnoDB DEFAULT CHARACTER SET = 'utf8mb4';\n\n"+
+		"SET foreign_key_checks=1;\n")
 
-	m.AddStructs(&Foo2{})
-
-	var buf bytes.Buffer
-	if err := m.Generate(&buf); err != nil {
-		t.Fatal(err)
-	}
-
-	got := buf.String()
-	want := "SET foreign_key_checks=0;\n" +
-		"DROP TABLE IF EXISTS `foo2`;\n\n" +
-		"CREATE TABLE `foo2` (\n" +
-		"    `id` INTEGER NOT NULL,\n" +
-		"    `name` VARCHAR(191) NOT NULL,\n" +
-		"    INDEX `idx_name` (`name`),\n" +
-		"    PRIMARY KEY (`id`)\n" +
-		") ENGINE=InnoDB DEFAULT CHARACTER SET = 'utf8mb4';\n\n" +
-		"SET foreign_key_checks=1;\n"
-	if got != want {
-		t.Errorf("want %q, got %q", want, got)
-	}
+	testMaker(t, []any{&Foo2{}}, "SET foreign_key_checks=0;\n"+
+		"DROP TABLE IF EXISTS `foo2`;\n\n"+
+		"CREATE TABLE `foo2` (\n"+
+		"    `id` INTEGER NOT NULL,\n"+
+		"    `name` VARCHAR(191) NOT NULL,\n"+
+		"    INDEX `idx_name` (`name`),\n"+
+		"    PRIMARY KEY (`id`)\n"+
+		") ENGINE=InnoDB DEFAULT CHARACTER SET = 'utf8mb4';\n\n"+
+		"SET foreign_key_checks=1;\n")
 }
